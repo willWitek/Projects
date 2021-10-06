142ffa12b88de9ff46869bb810aa09b70b24f561
willWitek <70545109+willWitek@users.noreply.github.com>
2021-10-06 14:49:13 -0400

    Add files via upload

---
+++ AddNoise.py
@@ -0,0 +1,44 @@
+#%%
+import numpy as np
+from scipy.fft import fft2, ifft2
+from PIL import Image
+import os
+from scipy.ndimage.filters import gaussian_filter
+
+maindir = '/Users/williamwitek/Documents/Fordham/JuniorYear/LeedsLab/imagenette_reconstructed/imagenette2/'
+cats = os.listdir(maindir)
+
+def generatenoise(img,dim):
+    f = np.fft.fftn(img)                                    #fourier transform of image
+    fPhase = np.angle(f)                                    #phase component of fft
+    fMag = np.abs(f)                                        #magnitude component of fft
+    
+    rng = np.random.default_rng()                           #rng seed
+    rng.shuffle(fPhase,0)                                   #shuffle phases in x 
+    rng.shuffle(fPhase,1)                                   #shuffle phases in y 
+
+    combined = np.multiply(np.abs(f), np.exp(1j * fPhase))  #recombine magnitude with shifted phases
+    imgCombined = np.real(np.fft.ifftn(combined))           #inverse fft to recreate original image
+    imgCombined = np.abs(imgCombined)                       #take absolute value of recombination to-
+                                                            #eliminate value clipping errors in final image
+
+    absfImg = Image.fromarray(imgCombined.astype('uint8'),'RGB') #convert phase-shifted noise array to PIL Image
+    absfImg = absfImg.resize(dim)                                #resize image to given dimensions
+    #absfImg.show('absf.png')   #optional show image
+    #absfImg.save('f2.png')     #optional save image
+    
+
+    return absfImg
+    
+   
+   
+testImg = Image.open(maindir + 'n03417042_118.JPEG')
+#testImg.show()
+testImg = np.array(testImg)
+dim = (1000,1000)
+
+for i in range(1):
+    generatenoise(testImg, dim)
+
+
+# %%
